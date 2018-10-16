0cc65c3b448627dc553863bb9a8a866a58eb053c
Araqsya <you@example.com>
2018-10-17 00:02:37 +0400

    changed files

---
+++ index.js
@@ -0,0 +1,21 @@
+// var obj={
+//     "first name":"Araqsya",
+//     "last name":"Nikoghosyan",
+//     "age":15,
+//     "tumo_student":true
+// }
+// var myjson = JSON.stringify(obj);
+// var fs = require('fs');
+
+// function main(){
+//    var file  = "obj.json";
+//    fs.appendFileSync(file, myjson);
+// }
+// main();
+var p5=require("p5")
+function keyPressed(){
+    console.log(key);
+  
+}
+  keyPressed()
+ 
